211344bf03682fc0f3c6c64b62a88a5b0cc39efe
Rodrigo Ruiz <rodrigoruiz@Rodrigos-Air-3.lan>
2026-04-14 14:25:19 -0400

    initial frontend

---
+++ web/app/wc/entry/new/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { useMemo, useState } from "react";
+
+export default function NewEntryPage() {
+  const [entryName, setEntryName] = useState("");
+  const [predictions, setPredictions] = useState({});
+
+  return <div>Create entry</div>;
+}
